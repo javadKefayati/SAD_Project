300ee42d96e9e3ad420918b260024e0787c3b791
taha7900 <taha7900@gmail.com>
2022-08-08 20:15:16 +0430

    models registered to admin page

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from core.models import File, FileLink, Library, LinkAccess
+
 # Register your models here.
+admin.site.register(Library)
+admin.site.register(File)
+admin.site.register(FileLink)
+admin.site.register(LinkAccess)
